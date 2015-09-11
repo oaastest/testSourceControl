4640c452a24535e063ec2ffcf760e673169d93b3
oaastest <oaastest@live.com>
2015-09-10 17:36:20 -0700

    Update Stop-AzVM.ps1

--- Stop-AzVM.ps1
+++ Stop-AzVM.ps1
@@ -3,18 +3,5 @@
 #>
 workflow Stop-AzVM {
 
-    # Names of the Azure VMs to stop
-    # TODO: replace with your Azure VM names
-    $VMsToStop = @("avmtotest2")
-
-    # Get creds to access Azure
-    $AzureCred = Get-AutomationPSCredential -Name "AzureCreds"
-
-    # Connect to Azure
-    $null = Add-AzureAccount -Credential $AzureCred
-
-    # Stop the VMs, in parallel
-    ForEach -Parallel ($VMName in $VMsToStop) {
-        Stop-AzureVM -Name $VMName -ServiceName $VMName -Force
-    }
+   
 }
